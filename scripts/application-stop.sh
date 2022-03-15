bb70626abb3708faf89a8379eec50990b551053d
Jeromy0515 <qkr07224@gmail.com>
2022-03-16 01:36:41 +0900

    Modify scripts

--- scripts/application-stop.sh
+++ scripts/application-stop.sh
@@ -3,4 +3,5 @@ if [[ -n "$(docker ps -a | grep was)" ]];
 then
   echo "Server is running"
   docker rm -f was
+  docker rmi 673306085779.dkr.ecr.ap-northeast-2.amazonaws.com/smc-badminton:latest
 fi
