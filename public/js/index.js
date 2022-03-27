e11b14dddecb235dbb4b0c65d9a1a304a0fb3e4f
wodnjse <macimaro04@gmail.com>
2022-03-28 00:43:08 +0900

    smc-badminton-sign-up-system-ver2

--- public/js/index.js
+++ public/js/index.js
@@ -2,20 +2,21 @@ const id = document.querySelector("#id"),
     psword = document.querySelector("#psword"),
     tel = document.querySelector("#tel");
 
+
 function login(){
     const req ={
-        id: id.value,
-        psword: psword.value,
-        tel: tel.value,
+        "name": document.getElementById('name').value,
+        "studentId": document.getElementById('studentId').value,
+        "phoneNumber": document.getElementById('tel').value
     };
     console.log(req);
     console.log(JSON.stringify(req))
 }
-// fetch("/log_in", {
-//     method: "POST",
-//     headers: {
-//         "Content-Type": ""
-//     },
-//     body: JSON.stringify()
-//     }
-// )
+fetch("/sign_up", {
+        method: "POST",
+        headers: {
+            Content-Type: "application/json"
+        },
+        body: JSON.stringify(req)
+    }
+)
