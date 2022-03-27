078c8934c1ba363d04c423a8d19fbb5cd9b279c7
wodnjse <macimaro04@gmail.com>
2022-03-28 01:27:49 +0900

    Update index

--- public/js/index.js
+++ public/js/index.js
@@ -11,13 +11,14 @@ function login(){
     };
     console.log(req);
     console.log(JSON.stringify(req))
-}
 
-fetch("/sign_up", {
+    fetch("/sign_up", {
         method: "POST",
         headers: {
             Content-Type: "application/json"
         },
         body: JSON.stringify(req)
     }
-)
+}
+
+
