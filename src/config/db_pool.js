153d9af19ac288dfc8e7bd639613942b66a38584
Jeromy0515 <qkr07224@gmail.com>
2022-03-28 14:40:48 +0900

    Refactoring backend logic and Modify file structure

---
+++ src/config/db_pool.js
@@ -0,0 +1,12 @@
+const mysql = require('mysql')
+
+module.exports =  {
+    createPool: () => {
+        return mysql.createPool({
+            host: 'mysql.clh0cegn7mjp.ap-northeast-2.rds.amazonaws.com',
+            user: 'admin',
+            password: 'iLy6wbTk795SXmw',
+            database: 'smc_badminton'
+        })
+    }
+}
