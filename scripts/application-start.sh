3bd563a47fdcac5141a3e560baa72575a30897e7
Jeromy0515 <qkr07224@gmail.com>
2022-03-31 12:57:55 +0900

    Update scripts

--- scripts/application-start.sh
+++ scripts/application-start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 aws ecr get-login-password --region ap-northeast-2 | docker login --username AWS --password-stdin 673306085779.dkr.ecr.ap-northeast-2.amazonaws.com
-docker run --name was -d -p 80:80 673306085779.dkr.ecr.ap-northeast-2.amazonaws.com/smc-badminton:latest
+docker run --name smc-badminton-was -d -p 80:80 673306085779.dkr.ecr.ap-northeast-2.amazonaws.com/smc-badminton:latest
