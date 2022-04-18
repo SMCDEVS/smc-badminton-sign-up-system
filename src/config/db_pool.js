d85767a0f42d746a0d85ac66763cd3e9ee355eff
Jeromy0515 <qkr07224@gmail.com>
2022-04-18 09:26:51 +0900

    remove database information

--- src/config/db_pool.js
+++ src/config/db_pool.js
@@ -3,10 +3,10 @@ const mysql = require('mysql')
 module.exports =  {
     createPool: () => {
         return mysql.createPool({
-            host: 'mysql.clh0cegn7mjp.ap-northeast-2.rds.amazonaws.com',
-            user: 'admin',
-            password: 'iLy6wbTk795SXmw',
-            database: 'smc_badminton',
+            host: 'hostname',
+            user: 'user',
+            password: 'password',
+            database: 'database',
             connectionLimit: 50
         })
     }
